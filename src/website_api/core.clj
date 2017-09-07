87dddda0e4202e88c3d30051c4ae3630f38742aa
Cody <helblingcody@gmail.com>
2017-09-06 20:40:37 -0600

    Got minimal server working

--- src/website_api/core.clj
+++ src/website_api/core.clj
@@ -4,7 +4,13 @@
             [ring.adapter.jetty :as jetty]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(compojure/defroutes app
+  (compojure/GET "/" [] {:body "Hello World!"
+                         :status 200
+                         :headers {"Content-Type" "text/plain"}}))
+
+(def wrapped-app (-> app
+                     (wrap-defaults site-defaults)))
+
+(defn -main []
+  (jetty/run-jetty #'wrapped-app {:join? false :port 8080}))
