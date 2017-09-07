273c6e722f66f467dcfeb7e37771f3185eb06a07
Cody <helblingcody@gmail.com>
2017-09-06 21:13:08 -0600

    Added static assets route

--- src/website_api/core.clj
+++ src/website_api/core.clj
@@ -1,13 +1,21 @@
 (ns website-api.core
   (:gen-class)
   (:require [compojure.core :as compojure]
+            [compojure.route :as route]
+            [compojure.handler :as handler]
+            [compojure.response :as response]
             [ring.adapter.jetty :as jetty]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (compojure/defroutes app
   (compojure/GET "/" [] {:body "Hello World!"
                          :status 200
-                         :headers {"Content-Type" "text/plain"}}))
+                         :headers {"Content-Type" "text/plain"}})
+  (compojure/GET "/test" [] {:body "This is a test!"
+                             :status 200
+                             :headers {"Content-Type" "text/plain"}})
+  (route/resources "/")
+  (route/not-found "Page not found"))
 
 (def wrapped-app (-> app
                      (wrap-defaults site-defaults)))
