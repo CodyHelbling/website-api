a6f454d3ef4641ae270f62347df6e958308d2996
Cody <helblingcody@gmail.com>
2017-11-30 06:11:41 -0700

    Init commit. Added basic example web service.

--- src/website_api/core.clj
+++ src/website_api/core.clj
@@ -5,17 +5,53 @@
             [compojure.handler :as handler]
             [compojure.response :as response]
             [ring.adapter.jetty :as jetty]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [monger.core :as mg]
+            [monger.collection :as mc]
+            [clojure.pprint :as pp]
+                        [monger.json]
+            [clojure.data.json :as json])
+  (:import [com.mongodb MongoOptions ServerAddress]
+           [org.bson.types ObjectId]))
+
+;(let [conn (mg/connect)])
+
+
+(defn test-write []
+  (let [conn (mg/connect)
+        db   (mg/get-db conn "monger-test")
+        coll "documents"]
+    (mc/insert-and-return db coll { :_id (ObjectId.) :username "ASDFADSFSAD" })))
+
+(defn test-read []
+  (let [conn (mg/connect)
+        db   (mg/get-db conn "monger-test")
+        coll "documents"
+        stuff (mc/find-maps db coll)]
+    (pp/pprint (:_id (first stuff)))
+    stuff))
+
+(defn test-remove []
+  (let [conn (mg/connect)
+        db   (mg/get-db conn "monger-test")
+        coll "documents"]
+    (mc/remove db coll)))
 
 (compojure/defroutes app
-  (compojure/GET "/" [] {:body "Hello World!"
-                         :status 200
-                         :headers {"Content-Type" "text/plain"}})
-  (compojure/GET "/test" [] {:body "This is a test!"
-                             :status 200
-                             :headers {"Content-Type" "text/plain"}})
-  (route/resources "/")
-  (route/not-found "Page not found"))
+    (compojure/GET "/" [] {:body "Hello World!"
+                           :status 200
+                           :headers {"Content-Type" "text/plain"}})
+    (compojure/GET "/test-write" [] {:body (str (test-write))
+                               :status 200
+                                     :headers {"Content-Type" "text/plain"}})
+    (compojure/GET "/test-read" [] {:body (json/write-str (test-read))
+                                    :status 200
+                                    :headers {"Content-Type" "text/plain"}})
+    (compojure/GET "/test-remove" [] {:body (str (test-remove))
+                               :status 200
+                               :headers {"Content-Type" "text/plain"}})  
+    (route/resources "/")
+    (route/not-found "Page not found"))
 
 (def wrapped-app (-> app
                      (wrap-defaults site-defaults)))
